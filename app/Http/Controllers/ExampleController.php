c4937dd716a56874c0f18475c81406813bd2731c
quentinmodena <quentinmodena@gmail.com>
2017-10-13 09:55:00 +0000

    fix merge conflits

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -18,18 +18,13 @@ class ExampleController extends Controller
 
     public function index()
     {
-<<<<<<< HEAD
+
         $test = array(
           array("marque" => "Volvo","prix" => 22,"cv" => 18),
           array("marque" => "BMW","prix" => 15,"cv" => 13),
           array("marque" => "Saab","prix" => 5,"cv" => 2),
           array("marque" => "Land Rover","prix" => 17,"cv" => 15)
         );
-=======
-        $test = User::login("root","0000");
->>>>>>> 8e566135623e72670d30d64319f719c973a14f93
         return json_encode($test);
     }
-
-    //
 }
